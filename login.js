c59d87d0b5e49e3ea53b8955b327458988b0ef42
GustavoGrosbelli <gustavogrosbelli26@gmail.com>
2025-03-13 15:05:55 -0300

    falta css

---
+++ login.js
@@ -0,0 +1,36 @@
+function lerEmail() {
+    var email = document.getElementById("meuEmail").value; // Pegando o valor do input
+    return email; 
+}
+function lerSenha(){
+    var senha = document.getElementById("minhaSenha").value;
+    return senha;
+
+}
+
+document.getElementById("mostrarSenha").addEventListener('click', function() {
+    var senhaInput = document.getElementById('minhaSenha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+        this.textContent = 'Ocultar Senha';
+    } else {
+        senhaInput.type = 'password';
+        this.textContent = 'Mostrar Senha';
+    }
+    });
+
+
+
+function cadastroFeito() {
+    var botao = document.getElementById("btnEntrar");
+
+    botao.addEventListener("click", function() {
+        var emailDigitado = lerEmail();
+        var senhaDigitada = lerSenha(); 
+        alert(" E-mail: " + emailDigitado + "\n Senha: "+senhaDigitada )
+
+        console.log("Email: " + emailDigitado + " Senha: " + senhaDigitada)
+    });
+    
+}
+cadastroFeito();
