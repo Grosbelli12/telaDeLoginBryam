b8148c57cbaca38ab28148219d893c094ecd6390
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-07-18 14:07:59 -0300

    ajustes na estilização da tela de login e implementação de controle de visibilidade para a tela de cadastro

--- AtvDustPizzaria/js/view_controller.js
+++ AtvDustPizzaria/js/view_controller.js
@@ -1,13 +1,16 @@
 // --- CONTROLES DE VISIBILIDADE DAS TELAS ---
+const loginContainer = document.querySelector('.login-container');
 const loginView = document.getElementById('login-view');
 const signupView = document.getElementById('signup-view');
 
 function showSignupView() {
     loginView.style.display = 'none';
     signupView.style.display = 'block';
+    loginContainer.classList.add('signup-active'); // Adiciona a classe para inverter
 }
 
 function showLoginView() {
     signupView.style.display = 'none';
     loginView.style.display = 'block';
+    loginContainer.classList.remove('signup-active'); // Remove a classe para voltar ao normal
 }
